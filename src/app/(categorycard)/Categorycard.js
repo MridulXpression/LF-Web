0280af9b2b408e3e76904a8bbd07c39c1883a142
Xpression-web <roshan.singh@xpressioncorp.com>
2025-12-17 15:07:50 +0530

    category

--- src/app/(categorycard)/Categorycard.js
+++ src/app/(categorycard)/Categorycard.js
@@ -1,40 +1,33 @@
 // CategorySection.jsx
 import React from "react";
-import CategoryCard from "@/components/CategoryCard";
+import ShopByCategory from "@/components/homepage/CategoryCardComponent";
 
 const CategorySection = () => {
   const categories = [
     {
-      title: "",
-      backgroundImage: "/images/women.png",
-      href: "/shop/women",
-    },
-    {
-      title: "",
-      backgroundImage: "/images/men.png",
+      id: 1,
+      name: "Men",
+      image: "/images/men.png",
       href: "/shop/men",
     },
     {
-      title: "",
-      backgroundImage: "/images/accessories.png",
+      id: 2,
+      name: "Women",
+      image: "/images/women.png",
+      href: "/shop/women",
+    },
+
+    {
+      id: 3,
+      name: "Accessories",
+      image: "/images/accessories.png",
       href: "/shop/accessories",
     },
   ];
 
   return (
-    <div className="bg-white px-4 py-8">
-      <div className="max-w-[1400px] mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-0 ">
-          {categories.map((category, index) => (
-            <CategoryCard
-              key={index}
-              title={category.title}
-              backgroundImage={category.backgroundImage}
-              href={category.href}
-            />
-          ))}
-        </div>
-      </div>
+    <div className="bg-white">
+      <ShopByCategory categories={categories} />
     </div>
   );
 };
