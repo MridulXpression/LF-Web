291d84f6b4125873f573f8747c3c4f636700bab9
Xpression-web <roshan.singh@xpressioncorp.com>
2026-01-30 09:55:58 +0530

    google sign in

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-gSLyUOzJ_bQ-wDz5x8R9AV4wY23Tcxs",
-  authDomain: "la-fetch-3b250.firebaseapp.com",
-  projectId: "la-fetch-3b250",
-  storageBucket: "la-fetch-3b250.firebasestorage.app",
-  messagingSenderId: "656354073125",
-  appId: "1:656354073125:web:981e0841a99cb63086fdcc",
-  measurementId: "G-5Z80WRYNWN",
+  apiKey: "AIzaSyDCeZ6Amos4mdFf9TzUoDGjnkWlArDBzW0",
+  authDomain: "lafetch-11b74.firebaseapp.com",
+  projectId: "lafetch-11b74",
+  storageBucket: "lafetch-11b74.firebasestorage.app",
+  messagingSenderId: "598072306679",
+  appId: "1:598072306679:web:19be0971bfb1dd1d8b1cf2",
+  measurementId: "G-MC1Z6D2Y3W",
 };
 
 // Initialize Firebase
